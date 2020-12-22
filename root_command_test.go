f1f0f51fce4973501b1d12366d48302febf11618
Mike Ball <mikedball@gmail.com>
2020-12-22 09:29:29 -0500

    move help tests to root_command_test.go

--- root_command_test.go
+++ root_command_test.go
@@ -1,37 +1,87 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os/exec"
+	"strings"
 	"testing"
 )
 
 func TestRootCommand(t *testing.T) {
 	tests := []struct {
-		arg            string
-		expectedOutput string
-		expectedErr    string
+		arg     string
+		outputs []string
+		err     error
 	}{{
-		arg:            "--foo",
-		expectedOutput: "Error: unknown flag: --foo\n",
-		expectedErr:    "exit status 1",
+		arg:     "--foo",
+		outputs: []string{"Error: unknown flag: --foo\n"},
+		err:     errors.New("exit status 1"),
 	}, {
-		arg:            "foo",
-		expectedOutput: "Error: unknown command \"foo\" for \"gossboss\"\nRun 'gossboss --help' for usage.\n",
-		expectedErr:    "exit status 1",
+		arg:     "foo",
+		outputs: []string{"Error: unknown command \"foo\" for \"gossboss\"\nRun 'gossboss --help' for usage.\n"},
+		err:     errors.New("exit status 1"),
+	}, {
+		arg: "--help",
+		outputs: []string{
+			"A tool for collecting goss test results from multiple goss servers' '/healthz' endpoints",
+			"Usage:",
+			"Available Commands:",
+			"Flags:",
+		},
+		err: nil,
+	}, {
+		arg: "-h",
+		outputs: []string{
+			"A tool for collecting goss test results from multiple goss servers' '/healthz' endpoints",
+			"Usage:",
+			"Available Commands:",
+			"Flags:",
+		},
+		err: nil,
+	}, {
+		arg: "",
+		outputs: []string{
+			"A tool for collecting goss test results from multiple goss servers' '/healthz' endpoints",
+			"Usage:",
+			"Available Commands:",
+			"Flags:",
+		},
+		err: nil,
+	}, {
+		arg: "--version",
+		outputs: []string{
+			"gossboss version",
+		},
+		err: nil,
+	}, {
+		arg: "-v",
+		outputs: []string{
+			"gossboss version",
+		},
+		err: nil,
 	}}
 
 	for _, test := range tests {
-		t.Run(fmt.Sprintf("when passed '%s'", test.arg), func(t *testing.T) {
+		t.Run(fmt.Sprintf("when gossboss is passed '%s'", test.arg), func(t *testing.T) {
 			output, err := exec.Command("./gossboss", test.arg).CombinedOutput()
 
-			if err.Error() != test.expectedErr {
-				t.Errorf("expected '%s' to error with '%s'; got '%s'", test.arg, test.expectedErr, err.Error())
+			if test.err == nil && err != nil {
+				t.Errorf("expected '%s' not to error; got '%v'", test.arg, err)
 			}
 
-			outStr := string(output)
-			if outStr != test.expectedOutput {
-				t.Errorf("expected '%s' to output '%s'; got '%s'", test.arg, test.expectedOutput, outStr)
+			if test.err != nil && err == nil {
+				t.Errorf("expected '%s' to error with '%s', but it didn't error", test.arg, test.err.Error())
+			}
+
+			if test.err != nil && err != nil && test.err.Error() != err.Error() {
+				t.Errorf("expected '%s' to error with '%s'; got '%s'", test.arg, test.err.Error(), err.Error())
+			}
+
+			for _, o := range test.outputs {
+				if !strings.Contains(string(output), o) {
+					t.Errorf("expected '%s' to include output '%s'; got '%s'", test.arg, o, output)
+				}
 			}
 		})
 	}
