6316ce8110b2cb9f855b438ad81472fdf600c0ce
Mike Ball <mikedball@gmail.com>
2021-02-05 07:10:29 -0500

    more granularly test stderr & stdout output

--- cmd/gossboss/serve_test.go
+++ cmd/gossboss/serve_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"net/http/httptest"
@@ -13,7 +14,6 @@ import (
 
 func TestServe(t *testing.T) {
 	var (
-		errExit         error  = errors.New("exit status 1")
 		description     string = "Collect and report goss test results from multiple goss servers' '/healthz' endpoints via a web server JSON endpoint"
 		placeholderText string = "REPLACE_ME"
 	)
@@ -48,19 +48,15 @@ func TestServe(t *testing.T) {
 		},
 		err: nil,
 	}, {
-		name: "when passed nothing",
-		arg:  "",
-		outputs: []string{
-			"Error: required flag(s) \"servers\" not set",
-		},
-		err: errExit,
+		name:    "when passed nothing",
+		arg:     "",
+		outputs: []string{},
+		err:     errors.New("Error: required flag(s) \"servers\" not set"),
 	}, {
-		name: "when passed a '--server' with no value",
-		arg:  "--servers",
-		outputs: []string{
-			"Error: flag needs an argument: --servers",
-		},
-		err: errExit,
+		name:    "when passed a '--server' with no value",
+		arg:     "--servers",
+		outputs: []string{},
+		err:     errors.New("Error: flag needs an argument: --servers"),
 	}}
 
 	for _, test := range tests {
@@ -74,18 +70,41 @@ func TestServe(t *testing.T) {
 				defer server.Close()
 			}
 
-			output, err := exec.Command("./gossboss", "serve", arg).CombinedOutput()
+			cmd := exec.Command("./gossboss", "serve", arg)
 
-			if test.err == nil && err != nil {
-				t.Errorf("expected '%s' not to error; got '%v'", arg, err)
+			stdOut, err := cmd.StdoutPipe()
+			if err != nil {
+				t.Errorf("expected creation of '%s' command stdout pipe not to error; got '%v'", arg, err)
 			}
 
-			if test.err != nil && err == nil {
-				t.Errorf("expected 'serve %s' to error with '%s', but it didn't error", arg, test.err.Error())
+			stdErr, err := cmd.StderrPipe()
+			if err != nil {
+				t.Errorf("expected creation of '%s' command stderr pipe not to error; got '%v'", arg, err)
 			}
 
-			if test.err != nil && err != nil && test.err.Error() != err.Error() {
-				t.Errorf("expected 'serve %s' to error with '%s'; got '%s'", arg, test.err.Error(), err.Error())
+			err = cmd.Start()
+			if err != nil {
+				t.Errorf("expected starting '%s' command not to error; got '%v'", arg, err)
+			}
+
+			outBuf := new(bytes.Buffer)
+			outBuf.ReadFrom(stdOut)
+			stdOutStr := outBuf.String()
+
+			errBuf := new(bytes.Buffer)
+			errBuf.ReadFrom(stdErr)
+			stdErrStr := errBuf.String()
+
+			if test.err == nil && stdErrStr != "" {
+				t.Errorf("expected '%s' not to output to stderr, but it output '%s'", arg, stdErrStr)
+			}
+
+			if test.err != nil && stdErrStr == "" {
+				t.Errorf("expected 'serve %s' stderr output to include '%s', but it didn't write to stderr", arg, test.err.Error())
+			}
+
+			if test.err != nil && stdErrStr != "" && !strings.Contains(stdErrStr, test.err.Error()) {
+				t.Errorf("expected 'serve %s' stderr output to include '%s'; got '%s'", arg, test.err.Error(), stdErrStr)
 			}
 
 			for _, o := range test.outputs {
@@ -93,8 +112,8 @@ func TestServe(t *testing.T) {
 					o = strings.ReplaceAll(o, placeholderText, server.URL+"/healthz")
 				}
 
-				if !strings.Contains(string(output), o) {
-					t.Errorf("expected 'serve %s' to include output '%s'; got '%s'", test.arg, o, output)
+				if !strings.Contains(string(stdOutStr), o) {
+					t.Errorf("expected 'serve %s' stdout output to include output '%s'; got '%s'", test.arg, o, stdOutStr)
 				}
 			}
 		})
